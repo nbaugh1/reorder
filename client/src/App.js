d2197a29b45c612a06a1a59f56e10b7945e531f8
nbaugh1 <nbaugh1@gmail.com>
2019-11-12 21:35:33 -0500

    moved dependecies

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+// import NavContainer from './containers/NavContainer';
+// import Home from './components/Home';
+// import { FromContainer } from './containers/FormContainer';
+// import { ItemsListContainer } from './containers/ItemsListContainer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  render() {
+  
+    return (
+      <Router>
+        {/* <NavContainer /> */}
+        <div className="container">
+          <Switch>
+            {/* <Route exact path="/" component={ Home } />
+            <Route exact path="/items/new" component={ FromContainer } />
+            <Route exact path="/items" component={ ItemsListContainer } /> */}
+            {/* <Route path="/lists" component={ ListsHistory } */}
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
 }
 
-export default App;
+export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux';
+// import rootReducer from './reducers/rootReducer.js'
 import App from './App';
+import thunk from 'react-thunk'
+
+const store = createStore(
+    // rootReducer,
+    compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+)
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
